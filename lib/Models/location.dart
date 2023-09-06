1f3cf8b966d53e831e0b61cffdf59e55ae706037
Mila Lekoska <mila.lekoska@students.finki.ukim.mk>
2023-09-06 20:17:25 +0200

    Adding location services, comunication with Google Maps and map locations

---
+++ lib/Models/location.dart
@@ -0,0 +1,21 @@
+import "package:flutter/material.dart";
+import "package:google_maps_flutter/google_maps_flutter.dart";
+
+class Location with ChangeNotifier {
+  final String id;
+  final String name;
+  final String address;
+  final LatLng coordinates;
+
+  Location({
+    required this.id,
+    required this.name,
+    required this.address,
+    required this.coordinates,
+  });
+
+  @override
+  String toString() {
+    return name;
+  }
+}
